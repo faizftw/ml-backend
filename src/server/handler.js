7e6564269e852b15baf0d99d1e2f77617979feed
faizftw <faizfatwa030206@gmail.com>
2024-05-22 05:17:08 +0000

    fifth

--- src/server/handler.js
+++ src/server/handler.js
@@ -3,24 +3,38 @@ const crypto = require('crypto');
 
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
+
+    if (image._data.length > 1000000) {
+        return h.response({
+            status: 'fail',
+            message: 'Payload content length greater than maximum allowed: 1000000'
+        }).code(413);
+    }
+
     const { model } = request.server.app;
 
-    const {label, suggestion } = await predictClassification(model, image);
-    const id = crypto.randomUUID();
-    const createdAt = new Date().toISOString();
- 
-  const data = {
-    "id": id,
-    "result": label,
-    "suggestion": suggestion,
-    "createdAt": createdAt
-  }
-  const response = h.response({
-    status: 'success',
-    message: 'Model is predicted successfully.',
-    data
-  })
-  response.code(201);
-  return response;
+    try {
+        const { label, suggestion } = await predictClassification(model, image._data);
+        const id = crypto.randomUUID();
+        const createdAt = new Date().toISOString();
+
+        const data = {
+            id: id,
+            result: label,
+            suggestion: suggestion,
+            createdAt: createdAt
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Model is predicted successfully.',
+            data
+        });
+        response.code(201);
+        return response;
+    } catch (error) {
+        throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
+    }
 }
+
 module.exports = postPredictHandler;
--- src/server/server.js
+++ src/server/server.js
@@ -1,50 +1,59 @@
 require('dotenv').config();
- 
+
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
- 
+
 (async () => {
     const server = Hapi.server({
         port: 8000,
         host: '0.0.0.0',
         routes: {
             cors: {
-              origin: ['*'],
+                origin: ['*'],
             },
+            payload: {
+                maxBytes: 1000000 
+            }
         },
     });
- 
+
     const model = await loadModel();
     server.app.model = model;
- 
+
     server.route(routes);
- 
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
+
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
                 message: `${response.message} Silakan gunakan foto lain.`
-            })
-            newResponse.code(response.statusCode)
+            });
+            newResponse.code(response.statusCode);
             return newResponse;
         }
- 
+
         if (response.isBoom) {
+            let message = 'Terjadi kesalahan dalam melakukan prediksi';
+
+            if (response.output.statusCode === 413) {
+                message = 'Payload content length greater than maximum allowed: 1000000';
+            }
+
             const newResponse = h.response({
                 status: 'fail',
-                message: response.message
-            })
-            newResponse.code(response.statusCode)
+                message: message
+            });
+            newResponse.code(response.output.statusCode);
             return newResponse;
         }
- 
+
         return h.continue;
     });
- 
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
