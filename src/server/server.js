14afebd8f10b8bcb5b518ce96f0d11e7c9176f8a
faizftw <faizfatwa030206@gmail.com>
2024-05-22 04:58:00 +0000

    third

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
