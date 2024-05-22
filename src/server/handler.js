d9253b3d8e9982872bae1a11279dc2038edd1828
faizftw <faizfatwa030206@gmail.com>
2024-05-22 11:05:49 +0000

    nineth

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,7 +1,8 @@
 const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
+const storeData = require('../services/storeData');
 
-async function postPredictHandler(request, h) {
+async function postPredictHandler(id, data) {
     const { image } = request.payload;
     const { model } = request.server.app;
 
@@ -16,6 +17,7 @@ async function postPredictHandler(request, h) {
             "suggestion": suggestion,
             "createdAt": createdAt
         }
+        await storeData(id, data);
         const response = h.response({
             status: 'success',
             message: 'Model is predicted successfully.',
